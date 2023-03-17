260cffcd96f285b54d3159d9efbce7291115e8ca
aditya1973c <105848081+aditya1973c@users.noreply.github.com>
2023-03-17 21:34:50 +0530

    fear man

---
+++ fear man/Assets/AdsManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+public class AdsManager : MonoBehaviour
+{
+
+    void Start()
+    {
+        Advertisement.Initialize("4814733");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
